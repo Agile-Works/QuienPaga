98c0ebf539285cf2f8ed13d7d02d3c12a4e658e3
Victoria <victoria.lavella@agileworks.com.uy>
2014-04-28 17:51:39 -0300

    cambios en las directivas

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,13 +1,15 @@
 'use strict';
 
-
 angular.module('quienPagaApp')
   .controller('MainCtrl', function ($scope) {
-	    var chart1 = {};
-	    chart1.type = 'PieChart';
-	    chart1.data = [ ['FA', 'cost'],['PN', 50000],['PC', 80000],['AP', 60000]];
-	    chart1.data.push(['PI',20000]);
-	    chart1.options = {displayExactValues: true,width: 400,height: 200,is3D: false,pieHole: 0.4,chartArea: {left:10,top:10,bottom:0,height:'100%'}};
-	    chart1.formatters = {number : [{columnNum: 1, pattern: '$ #,##0.00'}]};
-	    $scope.chart = chart1;
-});
+    var MainChart = {};
+    MainChart.methods={};
+    MainChart.type = 'PieChart';
+    MainChart.data = [ ['FA', 'cost'],['PN', 50000],['PC', 80000],['AP', 60000],['PI',20000]];
+    MainChart.options = {displayExactValues: true,width: 400,height: 200,is3D: false,pieHole: 0.4,chartArea: {left:10,top:10,bottom:0,height:'100%'}};
+    MainChart.formatters = {number : [{columnNum: 1, pattern: '$ #,##0.00'}]};
+    $scope.chart = MainChart;
+    $scope.chart.methods.select = function(selection, event){
+      console.log('chartSelect in ctrl, selection: '+selection + ' Evt: ' + event);
+    };
+  });
